80b60f4e823111f237fbee6e5d641c065377f983
cannoli9917 <katie.pennachio@gmail.com>
2019-03-02 14:34:10 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,71 @@
+let store = { drivers: [], passengers: [], trips: []}
+
+let driverId = 0
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId
+    this.name = name
+
+    store.drivers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id
+    })
+  }
+
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger()
+    })
+  }
+} //end of Driver class
+
+let passengerId = 0
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+
+    store.passengers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id
+    })
+  }
+
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver()
+    })
+  }
+} // end of Passenger class
+
+let tripId = 0
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+
+    store.trips.push(this)
+  }
+
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId
+    })
+  }
+
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId
+    })
+  }
+}//end of Trip class
